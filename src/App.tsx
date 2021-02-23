3b12272218c051cfaef7735ecea140dbd2010202
Thanush Praveen <thanushpraveen@aequalisys.com>
2021-02-23 14:43:52 +0530

    Sass and Font Implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./theme.scss";
+import "./App.scss";
+import { useTypedSelector } from "hooks/useTypedSelector";
+import LoadingPage from "components/View/UI/LoadingPage/LoadingPage";
+import { Switch } from "react-router-dom";
+import Layout from "components/Layout/Layout";
 
 function App() {
+  const [loading, setLoading] = useState<Boolean>(false);
+  const { theme } = useTypedSelector((state) => state.settings);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={`App ${theme}`}>
+      {loading ? (
+        <LoadingPage />
+      ) : (
+        <Layout>
+          <div className={`app-bg`}>
+            <div className={`bg-vector ${theme}`}>
+              <div
+                className="pt-6"
+                style={{ height: "100%", overflow: "auto", paddingTop: "60px" }}
+              >
+                <Switch></Switch>
+              </div>
+            </div>
+          </div>
+        </Layout>
+      )}
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.scss";
+import "./theme.scss";
 
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import { store } from "state";
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function

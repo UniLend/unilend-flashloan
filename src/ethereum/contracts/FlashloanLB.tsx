05e6beb563a67d49b8f5985735f6f3db2fe5fa59
Anand <anand@unilend.finance>
2021-03-05 17:12:12 +0530

    Contracts Setup for UI

---
+++ src/ethereum/contracts/FlashloanLB.tsx
@@ -0,0 +1,20 @@
+import web3 from "ethereum/web3";
+import { UnilendFlashLoanCoreContract } from ".";
+
+import UnilendFDonationABI from "../build/UnilendFDonation.json"
+import FlashloanABI from "../build/FlashLoanABI.json";
+import IERC20ABI from "../build/IERC20.json"
+
+export const FlashloanLBCore = new web3.eth.Contract(
+  FlashloanABI.abi,
+  UnilendFlashLoanCoreContract
+);
+export const UnilendFDonation = new web3.eth.Contract(
+  UnilendFDonationABI.abi,
+  UnilendFlashLoanCoreContract
+)
+
+export const IERC20 = new web3.eth.Contract(
+  IERC20ABI.abi,
+  UnilendFlashLoanCoreContract
+)
--- src/ethereum/contracts/index.tsx
+++ src/ethereum/contracts/index.tsx
@@ -1,3 +1,8 @@
+export const UnilendFlashLoanCoreContract: string =
+  "0x186b707bB603c16295eF38EA27a081EBf5b65989";
+
+export const Reciepent: string = "0x1434536899Da6e26B2AD6E5Cfd34611B47F993Ef";
+
 export const currencyList: {
   currency: {
     id: number;

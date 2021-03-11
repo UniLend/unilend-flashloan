731e5f6fcf09c593848b3438946d3b236429e72f
Thanush Praveen <thanushpraveen@aequalisys.com>
2021-03-11 16:37:04 +0530

    Conditional  Wallet Connect

--- src/state/action-creators/connectWalletAction.tsx
+++ src/state/action-creators/connectWalletAction.tsx
@@ -8,6 +8,108 @@ import { CoinbaseProvider, CoinbaseWeb3 } from "ethereum/coinbaseWeb3";
 import { formaticWeb3 } from "ethereum/formatic";
 import { portisWeb3 } from "ethereum/portis";
 
+async function handleWalletConnect(wallet: Wallet, dispatch: Dispatch<Action>) {
+  let accounts: any;
+  switch (wallet.name) {
+    case "metamask":
+      accounts = await web3Service.getAccounts();
+      if (window && !(window as any).ethereum.selectedAddress) {
+        (window as any).ethereum.enable().then(() => {
+          console.log("accounts", accounts);
+          dispatch({
+            type: ActionType.CONNECT_WALLET_SUCCESS,
+            payload: [...accounts],
+          });
+        });
+      } else {
+        dispatch({
+          type: ActionType.CONNECT_WALLET_SUCCESS,
+          payload: [...accounts],
+        });
+      }
+      break;
+    case "walletConnect":
+      try {
+        let provider: any = web3.connectWalletProvider;
+        console.log(provider);
+        await provider.enable();
+        accounts = await web3.connectWalletWeb3.eth.getAcoounts();
+        dispatch({
+          type: ActionType.CONNECT_WALLET_SUCCESS,
+          payload: [...accounts],
+        });
+      } catch (err) {
+        dispatch({
+          type: ActionType.CONNECT_WALLET_ERROR,
+          payload: err.message,
+        });
+      }
+      break;
+    case "CoinbaseWallet":
+      try {
+        CoinbaseProvider.enable().then((accounts: string[]) => {
+          console.log(`User's address is ${accounts[0]}`);
+          CoinbaseWeb3.eth.defaultAccount = accounts[0];
+          dispatch({
+            type: ActionType.CONNECT_WALLET_SUCCESS,
+            payload: [...accounts],
+          });
+        });
+      } catch (err) {
+        dispatch({
+          type: ActionType.CONNECT_WALLET_ERROR,
+          payload: err.message,
+        });
+      }
+      break;
+    case "Formatic":
+      try {
+        let web3: any = formaticWeb3;
+        console.log(
+          web3.currentProvider
+            .enable()
+            .then((res: any) => {
+              let address: string[];
+              address = res;
+              dispatch({
+                type: ActionType.CONNECT_WALLET_SUCCESS,
+                payload: [...address],
+              });
+            })
+            .catch((err: any) => {
+              dispatch({
+                type: ActionType.CONNECT_WALLET_ERROR,
+                payload: err.message,
+              });
+            })
+        );
+      } catch (err) {
+        dispatch({
+          type: ActionType.CONNECT_WALLET_ERROR,
+          payload: err.message,
+        });
+      }
+      break;
+    case "Portis":
+      try {
+        portisWeb3.eth.getAccounts((error, accounts) => {
+          let address: string[];
+          address = accounts;
+          dispatch({
+            type: ActionType.CONNECT_WALLET_SUCCESS,
+            payload: [...address],
+          });
+        });
+      } catch (err) {
+        dispatch({
+          type: ActionType.CONNECT_WALLET_ERROR,
+          payload: err.message,
+        });
+      }
+      break;
+  }
+}
+
 export const getAccountBalance = (selectedAccount: string) => {
   return async (dispatch: Dispatch<Action>) => {
     try {
@@ -31,77 +133,8 @@ export const connectWalletAction = (wallet?: Wallet) => {
     });
     try {
       console.log("WALLET", wallet);
-      let accounts: any;
-      if (wallet && wallet.name === "metamask") {
-        accounts = await web3Service.getAccounts();
-        if (window && !(window as any).ethereum.selectedAddress) {
-          (window as any).ethereum.enable().then(() => {
-            console.log("accounts", accounts);
-            dispatch({
-              type: ActionType.CONNECT_WALLET_SUCCESS,
-              payload: [...accounts],
-            });
-          });
-        } else {
-          dispatch({
-            type: ActionType.CONNECT_WALLET_SUCCESS,
-            payload: [...accounts],
-          });
-        }
-      } else if (wallet && wallet.name === "walletConnect") {
-        let provider: any = web3.connectWalletProvider;
-        console.log(provider);
-        await provider.enable();
-        accounts = await web3.connectWalletWeb3.eth.getAcoounts();
-        dispatch({
-          type: ActionType.CONNECT_WALLET_SUCCESS,
-          payload: [...accounts],
-        });
-      } else if (wallet && wallet.name === "CoinbaseWallet") {
-        CoinbaseProvider.enable().then((accounts: string[]) => {
-          console.log(`User's address is ${accounts[0]}`);
-          CoinbaseWeb3.eth.defaultAccount = accounts[0];
-          dispatch({
-            type: ActionType.CONNECT_WALLET_SUCCESS,
-            payload: [...accounts],
-          });
-        });
-      } else if (wallet && wallet.name === "Formatic") {
-        try {
-          let web3: any = formaticWeb3;
-          console.log(
-            web3.currentProvider
-              .enable()
-              .then((res: any) => {
-                let address: string[];
-                address = res;
-                dispatch({
-                  type: ActionType.CONNECT_WALLET_SUCCESS,
-                  payload: [...address],
-                });
-              })
-              .catch((err: any) => {
-                dispatch({
-                  type: ActionType.CONNECT_WALLET_ERROR,
-                  payload: err.message,
-                });
-              })
-          );
-        } catch (err) {
-          dispatch({
-            type: ActionType.CONNECT_WALLET_ERROR,
-            payload: err.message,
-          });
-        }
-      } else if (wallet && wallet.name === "Portis") {
-        portisWeb3.eth.getAccounts((error, accounts) => {
-          let address: string[];
-          address = accounts;
-          dispatch({
-            type: ActionType.CONNECT_WALLET_SUCCESS,
-            payload: [...address],
-          });
-        });
+      if (wallet) {
+        handleWalletConnect(wallet, dispatch);
       }
     } catch (err) {
       dispatch({

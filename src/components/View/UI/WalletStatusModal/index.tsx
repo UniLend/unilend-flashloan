ee0acaed2c0e84bc458a02979b08a717e82ad31f
Anand <anand@unilend.finance>
2021-03-11 12:00:12 +0530

    Wallet Status Modal

---
+++ src/components/View/UI/WalletStatusModal/index.tsx
@@ -0,0 +1,38 @@
+import { useTypedSelector } from "hooks/useTypedSelector";
+import { FC } from "react";
+import { Card, Container, Modal } from "react-bootstrap";
+import "./index.scss";
+interface Props {
+  handleClose: () => void;
+  address: string;
+}
+
+const WalletStateModal: FC<Props> = ({ handleClose, address }) => {
+  const { theme } = useTypedSelector((state) => state.settings);
+
+  return (
+    <>
+      <Modal
+        className={`modal-theme ${theme === "dark" ? "dark" : "light"}`}
+        animation={false}
+        size="sm"
+        show={true}
+        onHide={handleClose}
+      >
+        <Modal.Header closeButton>
+          <Modal.Title className="model-title-custom">Accounts</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Container>
+            <Card className="m-3 status-card">
+              <Card.Header>Connected to MetaMask</Card.Header>
+              <Card.Body>{address}</Card.Body>
+            </Card>
+          </Container>
+        </Modal.Body>
+      </Modal>
+    </>
+  );
+};
+
+export default WalletStateModal;

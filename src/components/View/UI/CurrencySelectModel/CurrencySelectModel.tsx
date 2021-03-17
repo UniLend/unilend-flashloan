f8700b3d7f94cce98a695c28ec779cf959857c23
Thanush Praveen <thanushpraveen@aequalisys.com>
2021-03-17 18:13:18 +0530

    CurrencySelectModel Sub Tab Toggle

--- src/components/View/UI/CurrencySelectModel/CurrencySelectModel.tsx
+++ src/components/View/UI/CurrencySelectModel/CurrencySelectModel.tsx
@@ -9,17 +9,12 @@ import { Heading5, Heading6 } from "components/View/UI/widgets/Common";
 import EditIcon from "assets/edit.svg";
 import backIcon from "assets/back.svg";
 import Manage from "./Manage";
+import { useActions } from "hooks/useActions";
 // ! Let React Handle Keys
 interface Props {
   handleClose: () => void;
-  currFieldName: string;
-  handleCurrChange: (selectedField: any) => void;
-}
-
-interface FList {
-  id: number;
-  name: string;
-  desc: string;
+  currFieldName: any;
+  handleCurrChange: (selectedField: string, selectedLogo: any) => void;
 }
 
 const CurrencySelectModel: FC<Props> = ({
@@ -30,18 +25,28 @@ const CurrencySelectModel: FC<Props> = ({
   const { theme } = useTypedSelector((state) => state.settings);
 
   const [searchText, setSearchText] = useState<string>("");
-  const [filteredList, setFilteredList] = useState<Array<FList>>([]);
+  const [filteredList, setFilteredList] = useState([{}]);
   const [openManage, setOpenManage] = useState<Boolean>(false);
+  const { tokenList } = useTypedSelector((state) => state.tokenManage);
+  const { fetchTokenList } = useActions();
 
   useEffect(() => {
-    let filteredList = [...currencyList.currency];
-    if (searchText.trim().length > 0)
-      filteredList = filteredList.filter(
-        (e) => searchWord(e.name, searchText) || searchWord(e.desc, searchText)
-      );
-    setFilteredList(filteredList);
+    fetchTokenList();
+  }, []);
+
+  useEffect(() => {
+    if (tokenList.payload.length) {
+      let filteredList = tokenList.payload;
+      console.log(filteredList);
+      if (searchText.trim().length > 0)
+        filteredList = filteredList.filter((e) => {
+          // console.log(e);
+          // searchWord(e.name, searchText) || searchWord(e.desc, searchText);
+        });
+      setFilteredList(filteredList);
+    }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [currencyList.currency, searchText]);
+  }, [currencyList.currency, searchText, tokenList]);
 
   const SearchBar = (
     <div style={{ margin: " 15px auto 0 auto" }}>
@@ -68,38 +73,52 @@ const CurrencySelectModel: FC<Props> = ({
   // const ManageBodyContent = <div></div>;
   const MainBodyContent = (
     <div className="curr-list-group">
-      <ListGroup>
-        {Children.toArray(
-          filteredList.map((item: any) => (
-            <ListGroup.Item
-              key={item.id}
-              action
-              onClick={() => handleCurrChange(item.name)}
-            >
-              <div className="row">
-                <div className="col-2 px-0 curr-list">
-                  <img width="24" className="list-icon" src={Logo} alt="" />
-                </div>
-                <div className="col-10">
-                  <div className="row">
-                    <h6 className="mb-0" style={{ textTransform: "uppercase" }}>
-                      {item.name}
-                    </h6>
+      {filteredList.length ? (
+        <ListGroup>
+          {Children.toArray(
+            filteredList.map((item: any) => (
+              <ListGroup.Item
+                key={item.id}
+                action
+                onClick={() => handleCurrChange(item.symbol, item.logoURI)}
+              >
+                <div className="row">
+                  <div className="col-2 px-0 curr-list">
+                    <img
+                      width="24"
+                      className="list-icon"
+                      src={item.logoURI}
+                      alt=""
+                    />
                   </div>
-                  <div className="row">
-                    <p
-                      className="mb-0 list-desc"
-                      style={{ textTransform: "capitalize" }}
-                    >
-                      {item.desc}
-                    </p>
+                  <div className="col-10">
+                    <div className="row">
+                      <h6
+                        className="mb-0"
+                        style={{ textTransform: "uppercase" }}
+                      >
+                        {item.symbol}
+                      </h6>
+                    </div>
+                    <div className="row">
+                      <p
+                        className="mb-0 list-desc"
+                        style={{ textTransform: "capitalize" }}
+                      >
+                        {item.name}
+                      </p>
+                    </div>
                   </div>
                 </div>
-              </div>
-            </ListGroup.Item>
-          ))
-        )}
-      </ListGroup>
+              </ListGroup.Item>
+            ))
+          )}
+        </ListGroup>
+      ) : (
+        <>
+          <p>Fetching List</p>
+        </>
+      )}
     </div>
   );
   return (
@@ -158,7 +177,9 @@ const CurrencySelectModel: FC<Props> = ({
           </Modal.Title>
           {!openManage && SearchBar}
         </Modal.Header>
-        <Modal.Body className={openManage && "manage"}>{openManage ? <Manage /> : MainBodyContent}</Modal.Body>
+        <Modal.Body className={openManage && "manage"}>
+          {openManage ? <Manage /> : MainBodyContent}
+        </Modal.Body>
         {!openManage && <Modal.Footer>{ManageButton}</Modal.Footer>}
       </Modal>
     </>
--- src/components/View/UI/CurrencySelectModel/Manage/index.tsx
+++ src/components/View/UI/CurrencySelectModel/Manage/index.tsx
@@ -2,6 +2,9 @@ import React, { useEffect, useState } from "react";
 import { useTypedSelector } from "hooks/useTypedSelector";
 import { FC } from "react";
 import { Button, Col, Container, Row } from "react-bootstrap";
+import { useActions } from "hooks/useActions";
+import useWalletConnect from "hooks/useWalletConnect";
+import { IERC20 } from "ethereum/contracts/FlashloanLB";
 
 interface Props {}
 
@@ -11,17 +14,31 @@ const Manage: FC<Props> = (props) => {
   const [searchText, setSearchText] = useState<string>("");
 
   const [activeSubTab, setActiveSubTab] = useState<string>("list");
+  const { currentProvider } = useWalletConnect();
 
   const { theme } = useTypedSelector((state) => state.settings);
   const { payload: tokenList, isRequesting } = useTypedSelector(
     (state) => state.tokenManage.tokenList
   );
+  const { fetchTokenList } = useActions();
   useEffect(() => {
     console.log(tokenList);
-  });
+  }, [tokenList]);
 
-  const handleActiveToggle = () => {};
+  const handleActiveToggle = () => {
+    setActiveSubTab(activeSubTab === "list" ? "token" : "list");
+  };
+  const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchText(e.target.value);
 
+    let IERC = IERC20(currentProvider);
+    // IRC.methods.name(e.target.value).call((err: any, res: any) => {
+    //   if (!err && res) {
+    //     console.log(res);
+    //   }
+    // });
+    fetchTokenList();
+  };
   return (
     <>
       <Container className="p-0">
@@ -33,32 +50,46 @@ const Manage: FC<Props> = (props) => {
           onClick={handleActiveToggle}
         >
           <Row>
-            <Col className="manage-tab active">List</Col>
-            <Col className="manage-tab">Token</Col>
+            <Col
+              className={`manage-tab ${activeSubTab === "list" && "active"}`}
+            >
+              List
+            </Col>
+            <Col
+              className={`manage-tab ${activeSubTab === "token" && "active"}`}
+            >
+              Token
+            </Col>
           </Row>
         </Button>
       </Container>
-      <div>
-        <input
-          type="text"
-          value={searchText}
-          className="form-control model-search-input"
-          placeholder="https:// or ipfs:// or ENS name"
-          onChange={(e) => setSearchText(e.target.value)}
-        />
-      </div>
-      <div>
-        {isRequesting || tokenList.length === 0 ? (
-          <span>{isRequesting ? "Loading..." : "No tokens found."}</span>
-        ) : (
-          <>
-            {/* {tokenList.length &&
+      {activeSubTab === "token" ? (
+        <>
+          <div>
+            <input
+              type="text"
+              value={searchText}
+              className="form-control model-search-input"
+              placeholder="https:// or ipfs:// or ENS name"
+              onChange={handleSearch}
+            />
+          </div>
+          <div>
+            {isRequesting || tokenList.length === 0 ? (
+              <span>{isRequesting ? "Loading..." : "No tokens found."}</span>
+            ) : (
+              <>
+                {/* {tokenList.length &&
               tokenList.map((item: any, i: number) => (
                 <div key={i}>{item.name}</div>
               ))} */}
-          </>
-        )}
-      </div>
+              </>
+            )}
+          </div>
+        </>
+      ) : (
+        <div></div>
+      )}
     </>
   );
 };

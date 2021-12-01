1ed4b9a1b0f434ee327668075a352c5aa0533030
Thanush Praveen <thanushpraveen@aequalisys.com>
2021-12-01 11:32:29 +0530

    Moonriver logo updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,22 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import React, { Dispatch, useEffect, useState } from "react";
-import "./theme.scss";
-import "./App.scss";
-import { useTypedSelector } from "hooks/useTypedSelector";
-import LoadingPage from "components/View/UI/LoadingPage/LoadingPage";
-import { Redirect, Route, Switch, useHistory } from "react-router-dom";
-import Layout from "components/Layout/Layout";
-import dotEnv from "dotenv";
+import React, { Dispatch, useEffect, useState } from 'react'
+import './theme.scss'
+import './App.scss'
+import { useTypedSelector } from 'hooks/useTypedSelector'
+import LoadingPage from 'components/View/UI/LoadingPage/LoadingPage'
+import { Redirect, Route, Switch, useHistory } from 'react-router-dom'
+import Layout from 'components/Layout/Layout'
+import dotEnv from 'dotenv'
 // import useWalletConnect from "hooks/useWalletConnect";
-import CommonCard from "components/View/CommonCard";
-import useWalletConnect from "hooks/useWalletConnect";
-import { Alert } from "react-bootstrap";
-import AlertImg from "assets/warning.svg";
-import BigNumber from "bignumber.js";
-import { errorHandler } from "index";
-import { useActions } from "hooks/useActions";
-import { SettingAction } from "state/actions/settingsA";
-import { useDispatch } from "react-redux";
+import CommonCard from 'components/View/CommonCard'
+import useWalletConnect from 'hooks/useWalletConnect'
+import { Alert } from 'react-bootstrap'
+import AlertImg from 'assets/warning.svg'
+import BigNumber from 'bignumber.js'
+import { errorHandler } from 'index'
+import { useActions } from 'hooks/useActions'
+import { SettingAction } from 'state/actions/settingsA'
+import { useDispatch } from 'react-redux'
 // import { useActions } from "hooks/useActions";
 // declare const window: any;
 // interface ProviderMessage {
@@ -27,56 +27,51 @@ import { useDispatch } from "react-redux";
 BigNumber.config({
   EXPONENTIAL_AT: 1000,
   DECIMAL_PLACES: 80,
-});
+})
 
 function App() {
-  const [loading, setLoading] = useState<Boolean>(true);
+  const [loading, setLoading] = useState<Boolean>(true)
 
-  const [alertShow, setAlertShow] = useState<Boolean>(true);
-  const history = useHistory();
+  const [alertShow, setAlertShow] = useState<Boolean>(true)
+  const history = useHistory()
 
-  const { theme, activeTab } = useTypedSelector((state) => state.settings);
-  const { tokenByUrl } = useTypedSelector((state) => state.tokenManage);
-  const { handleTokenPersist, handleCustomTokens } = useActions();
-  const {
-    handleWalletConnect,
-    walletProvider,
-    selectedNetworkId,
-    connectedWallet,
-  } = useWalletConnect();
-  const { setActiveTab } = useActions();
+  const { theme, activeTab } = useTypedSelector((state) => state.settings)
+  const { tokenByUrl } = useTypedSelector((state) => state.tokenManage)
+  const { handleTokenPersist, handleCustomTokens } = useActions()
+  const { handleWalletConnect, walletProvider, selectedNetworkId, connectedWallet } = useWalletConnect()
+  const { setActiveTab } = useActions()
 
-  const dispatch = useDispatch<Dispatch<SettingAction>>();
+  const dispatch = useDispatch<Dispatch<SettingAction>>()
 
   useEffect(() => {
-    dispatch(setActiveTab(history.location.pathname.slice(1)));
-  }, [history]);
+    dispatch(setActiveTab(history.location.pathname.slice(1)))
+  }, [history])
 
   useEffect(() => {
-    if (localStorage.getItem("activeTab")) {
-      dispatch(setActiveTab(localStorage.getItem("activeTab")));
+    if (localStorage.getItem('activeTab')) {
+      dispatch(setActiveTab(localStorage.getItem('activeTab')))
     }
-    dotEnv.config();
-    handleCustomTokens();
+    dotEnv.config()
+    handleCustomTokens()
     setTimeout(() => {
-      setLoading(false);
-    }, 2000);
-    console.log("starting");
+      setLoading(false)
+    }, 2000)
+    console.log('starting')
     errorHandler.start({
       key: process.env.REACT_APP_GOOLE_CLOUD_LOGGING_API,
       projectId: process.env.REACT_APP_GOOLE_CLOUD_LOGGING_PROJECTID,
-    });
-  }, []);
+    })
+  }, [])
 
   useEffect(() => {
-    handleTokenPersist(tokenByUrl, selectedNetworkId);
-  }, [tokenByUrl, selectedNetworkId]);
+    handleTokenPersist(tokenByUrl, selectedNetworkId)
+  }, [tokenByUrl, selectedNetworkId])
 
   useEffect(() => {
     if (connectedWallet) {
-      handleWalletConnect(JSON.parse(connectedWallet));
+      handleWalletConnect(JSON.parse(connectedWallet))
     }
-  }, [walletProvider, connectedWallet]);
+  }, [walletProvider, connectedWallet])
 
   return (
     <div className={`App ${theme}`}>
@@ -98,20 +93,20 @@ function App() {
                   >
                     {` audited by Certik`}
                   </a>
-                  . Please familiarize yourself with the platform to understand
-                  the correct usage and features of the platform.
+                  . Please familiarize yourself with the platform to understand the correct usage and features of the
+                  platform.
                 </p>
               </div>
             </Alert>
           )}
           <Layout>
-            <div className={`app-bg ${alertShow ? "height-92" : "height-100"}`}>
+            <div className={`app-bg ${alertShow ? 'height-92' : 'height-100'}`}>
               <div className={`bg-vector ${theme}`}>
                 <div
                   className="pt-6"
                   style={{
-                    height: "100%",
-                    overflow: "auto",
+                    height: '100%',
+                    overflow: 'auto',
                     // paddingTop: "60px",
                   }}
                 >
@@ -133,7 +128,7 @@ function App() {
         </>
       )}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App

4e09c9e804ed78c2ffe6fb44aa1880bec5201966
Anil Pujari <anilpujari@Anils-MacBook-Air.local>
2023-04-07 10:36:20 +0530

    redux-logger added

--- src/state/store.tsx
+++ src/state/store.tsx
@@ -1,10 +1,11 @@
 import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
+import logger from 'redux-logger'
 import reducers from "./reducers";
 import persistStore from "redux-persist/es/persistStore";
 // const composeEnhancers = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
 
-export const store = createStore(reducers, applyMiddleware(thunk));
+export const store = createStore(reducers, applyMiddleware( logger ,thunk));
 
 export const persistor = persistStore(store);
 // composeEnhancers(applyMiddleware(thunk))

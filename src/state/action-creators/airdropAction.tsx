dc5f037fbdefca1e8a0af347a9436d29f56e318e
Mangeshbk <mangeshbkwsu@gmail.com>
2023-04-26 16:09:36 +0530

    update airDrop method with ethers js

--- src/state/action-creators/airdropAction.tsx
+++ src/state/action-creators/airdropAction.tsx
@@ -5,6 +5,8 @@ import { errorHandler } from 'index'
 import { Dispatch } from 'react'
 import { ActionType } from 'state/action-types'
 import { AirdropAction } from 'state/actions/airdropA'
+import { fetchSigner, getContract, getNetwork, getProvider, waitForTransaction } from 'wagmi/dist/actions'
+import IERC20ABI from 'ethereum/build/IERC20.json'
 
 export const setAirdropSuccess = () => {
   return async (dispatch: Dispatch<AirdropAction>) => {
@@ -14,11 +16,48 @@ export const setAirdropSuccess = () => {
   }
 }
 
+const getContractInstance = async (contractAddress: any, abi: any, signerData: any) => {
+  try {
+    let signer = signerData
+    if (signer === null) {
+      signer = await fetchSigner()
+    }
+    const provider = getProvider()
+    const instance = getContract({
+      address: contractAddress, //
+      abi, //
+      signerOrProvider: signer || provider,
+    })
+    return instance
+  } catch (error) {
+    throw error
+  }
+}
+const checkTxnStatus = async (hash: any) => {
+  try {
+    const receipt = waitForTransaction({
+      hash,
+    })
+
+    if ((await receipt).status === 1) {
+      return true
+    } else {
+      setTimeout(async () => {
+        checkTxnStatus(hash)
+      }, 1000)
+    }
+  } catch (error) {
+    setTimeout(async () => {
+      checkTxnStatus(hash)
+    }, 1000)
+  }
+}
+
 export const handleAirdrop = (
   currentProvider: any,
   amount: any,
   account: any,
-  reciepentAddress: string,
+  tokenAddress: string,
   isEth: boolean,
   decimal: any,
   selectedNetworkId: any,
@@ -29,33 +68,58 @@ export const handleAirdrop = (
     })
     try {
       var fullAmount = web3Service.getValue(isEth, currentProvider, amount, decimal)
+      const { chain } = getNetwork()
+      const signer = await fetchSigner()
+      // const instance = await getContractInstance(UnilendFlashLoanCoreContract('', chain?.id), IERC20ABI.abi, signer)
+      console.log('TOKEN_ADDRESS', tokenAddress)
+      console.log('FLASH_LOAN_ADDRESS', UnilendFlashLoanCoreContract('', chain?.id))
+      const instance = await getContractInstance(tokenAddress, IERC20ABI.abi, signer) // ER20 instance
+      // tokenAddress : check is it Token Address?
+      console.log('TSX_INSTANCE', instance)
+      const txs = await instance.transfer(UnilendFlashLoanCoreContract('', chain?.id), fullAmount)
+      // const txs = await instance.transfer(reciepentAddress, fullAmount)
+      console.log('TSX', txs)
 
-      await IERC20(currentProvider, reciepentAddress)
-        .methods.transfer(UnilendFlashLoanCoreContract(currentProvider, selectedNetworkId), fullAmount)
-        .send({
-          from: account,
-          gasPrice: defaultGasPrice * 1e9,
-        })
-        .on('receipt', (res: any) => {
+      if (txs.hash) {
+        const status = await checkTxnStatus(txs.hash)
+        if (status) {
           dispatch({
             type: ActionType.AIRDROP_SUCCESS,
           })
-        })
-        .on('transactionHash', (hash: any) => {
           dispatch({
             type: ActionType.AIRDROP_TRANSACTION_HASH,
-            payload: hash,
+            payload: txs.hash,
           })
-        })
-        .on('error', (err: any, res: any) => {
-          errorHandler.report(err)
+        }
+      }
 
-          dispatch({
-            type: ActionType.AIRDROP_FAILED,
-            message: res === undefined ? 'Transaction Rejected' : 'Transaction Failed',
-          })
-        })
+      // await IERC20(currentProvider, reciepentAddress)
+      //   .methods.transfer(UnilendFlashLoanCoreContract(currentProvider, selectedNetworkId), fullAmount)
+      //   .send({
+      //     from: account,
+      //     gasPrice: defaultGasPrice * 1e9,
+      //   })
+      //   .on('receipt', (res: any) => {
+      //     dispatch({
+      //       type: ActionType.AIRDROP_SUCCESS,
+      //     })
+      //   })
+      //   .on('transactionHash', (hash: any) => {
+      //     dispatch({
+      //       type: ActionType.AIRDROP_TRANSACTION_HASH,
+      //       payload: hash,
+      //     })
+      //   })
+      //   .on('error', (err: any, res: any) => {
+      //     errorHandler.report(err)
+
+      //     dispatch({
+      //       type: ActionType.AIRDROP_FAILED,
+      //       message: res === undefined ? 'Transaction Rejected' : 'Transaction Failed',
+      //     })
+      //   })
     } catch (error) {
+      console.log('CATCH_ERROR', error)
       dispatch({
         type: ActionType.AIRDROP_FAILED,
         message: 'Transaction Failed',
